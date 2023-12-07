9f99f7319e4225df8f6f3f085d3ab34bcb6e559f
bentottts <ryvndngca@gmail.com>
2023-12-07 11:52:26 +0800

    enhancement

--- randomizer-app/src/main/java/com/awsm/api/randomizerapp/controller/HomeController.java
+++ randomizer-app/src/main/java/com/awsm/api/randomizerapp/controller/HomeController.java
@@ -1,17 +1,14 @@
 package com.awsm.api.randomizerapp.controller;
 
 import com.awsm.api.randomizerapp.exception.EmptyRegistryException;
-import com.awsm.api.randomizerapp.model.Candidate;
 import com.awsm.api.randomizerapp.service.CandidateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-import java.util.List;
+
 
 @Slf4j
 @Controller
@@ -25,20 +22,14 @@ public class HomeController {
     }
 
     @GetMapping
-    public String home(Model model) throws InterruptedException, EmptyRegistryException {
+    public String home() throws InterruptedException, EmptyRegistryException {
+        return "dashboard";
+    }
+
+    @GetMapping("/roll")
+    public String scanner(Model model){
         model.addAttribute("candidates", candidateService.getCandidates());
-        return "index";
-
+        return "roll";
     }
 
-    @GetMapping("/showWinner")
-    public String showWinner(Model model) throws InterruptedException {
-
-        //get winner
-        Candidate candidate = candidateService.getWinner();
-        log.info(candidate.toString());
-
-        model.addAttribute("candidate", candidateService);
-        return "index";
-    }
 }
